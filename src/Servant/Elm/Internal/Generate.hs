2da0218c405c09551168d43c4e5e4ba5ce6d651c
Clifford Meeks <cliffmeeks2214@gmail.com>
2018-09-05 13:02:21 -0500

    [elm-bridge] unbroke build

--- src/Servant/Elm/Internal/Generate.hs
+++ src/Servant/Elm/Internal/Generate.hs
@@ -147,7 +147,7 @@ generateElmModuleWith options namespace imports rootDir typeDefs api = do
         [ T.pack $ Elm.moduleHeader Elm0p18 moduleName
         , ""
         , imports
-        , T.pack $ Elm.makeModuleContentWithAlterations options typeDefs
+        , T.pack $ Elm.makeModuleContentWithAlterations (options elmAlterations) typeDefs
         ] ++
         generateElmForAPIWith options api
       moduleName = intercalate "." namespace
